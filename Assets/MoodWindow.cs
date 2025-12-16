fb49df9af45da4b1763e45f37e36a38aa03396bb
micdaniele <micocci.daniele@gmail.com>
2025-12-16 23:09:52 +0100

    risoluzione errori script e telecamera

--- Assets/MoodWindow.cs
+++ Assets/MoodWindow.cs
@@ -2,22 +2,30 @@ using UnityEngine;
 
 public class MoodWindow : MonoBehaviour
 {
-    //public GameObject moodContent;
-    //public GameObject recipeContent;
+    public GameObject moodContent;
+    public GameObject recipeContent;
 
-    //public void SelectMood(string mood)
-    //{
-    //    GameManager.Instance.SetMood(mood);
+    // Metodo chiamato quando viene selezionato un mood
+    public void SelectMood(int mood)
+    {
+        GameManager.Instance.SetMood(mood);
 
-    //    moodContent.SetActive(false);
-    //    recipeContent.SetActive(true);
+        moodContent.SetActive(false);
+        recipeContent.SetActive(true);
 
-    //    GameManager.Instance.PrepareRecipes();
-    //}
+        GameManager.Instance.PrepareRecipes();
+    }
 
-    //public void SelectRecipe(string recipe)
-    //{
-    //    GameManager.Instance.SetRecipe(recipe);
-    //    Hide();
-    //}
+    // Metodo chiamato quando viene selezionata una ricetta
+    public void SelectRecipe(string recipe)
+    {
+        GameManager.Instance.SetRecipe(recipe);
+        Hide();
+    }
+
+    // Metodo per nascondere la finestra
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
 }
