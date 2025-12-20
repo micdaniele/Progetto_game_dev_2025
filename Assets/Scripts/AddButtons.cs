63fff21349e632b92e77f59f40ee792d92867cac
v11tto <vittorio.lerario29@gmail.com>
2025-12-20 15:09:55 +0100

    continuo memory

---
+++ Assets/Scripts/AddButtons.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+
+    [SerializeField]
+    private GameObject btn;
+
+    void Awake() {
+
+        for (int i=0; i < 11; i++)
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+
+}
+
+}
