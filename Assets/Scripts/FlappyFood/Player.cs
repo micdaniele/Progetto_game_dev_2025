0a5b7d1dff105bf7268784ad4145a05f320ace84
Chiara Rosi <chiararosi2003@gmail.com>
2026-01-21 01:21:18 +0100

    la mia parte del gioco, cambiate nomi o quello che volete

---
+++ Assets/Scripts/FlappyFood/Player.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+    private Vector3 direction;
+    public float gravity = -9.8f;
+    public float strength = 5f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
+    private void Update()
+    {
+        //space o tasto sinistro fanno battere le ali all'uccello
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            direction = Vector3.up * strength;
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                direction = Vector3.up * strength;
+            }
+        }
+        direction.y += gravity * Time.deltaTime;
+        transform.position += direction * Time.deltaTime;//agiorna la posizione del nostro uccello 
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+        if(spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
+    }
+}
