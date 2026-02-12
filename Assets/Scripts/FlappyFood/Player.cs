34bef258172a4e94ba422a04291707c8221629b8
v11tto <vittorio.lerario29@gmail.com>
2026-02-12 16:03:23 +0100

    leggere sotto
    nel gioco flappyfood ci stava anche la possibilita di far saltare il pollo usando il touch (se fosse un cellulare) ho tolto questa funzionalita dato che il gioco è solo per pc

--- Assets/Scripts/FlappyFood/Player.cs
+++ Assets/Scripts/FlappyFood/Player.cs
@@ -34,15 +34,7 @@ public class Player : MonoBehaviour
         {
             direction = Vector3.up * strength;
         }
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
 
-            if(touch.phase == TouchPhase.Began)
-            {
-                direction = Vector3.up * strength;
-            }
-        }
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;//agiorna la posizione del nostro uccello 
     }
